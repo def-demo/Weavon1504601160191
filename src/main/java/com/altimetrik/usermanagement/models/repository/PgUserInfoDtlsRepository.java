730ddea844099bd918176666876df0d1be61e11b
sghosh <sghosh@altimetrik.com>
2017-09-05 14:16:03 +0530

    Initial commit

---
+++ src/main/java/com/altimetrik/usermanagement/models/repository/PgUserInfoDtlsRepository.java
@@ -0,0 +1,23 @@
+package com.altimetrik.usermanagement.models.repository;
+
+import com.altimetrik.usermanagement.models.*;
+
+import com.altimetrik.usermanagement.models.PgSkillTrackDtls;
+import com.altimetrik.usermanagement.models.PgSourceMstr;
+import com.altimetrik.usermanagement.models.PgRequirement;
+import java.util.Date;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+
+@Repository
+public interface PgUserInfoDtlsRepository extends JpaRepository<PgUserInfoDtls, Long>{
+
+}
+
+
+
